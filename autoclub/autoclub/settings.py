2ff2cb4cc2c1873147552630c1a28f72967adebd
vokator <vokator@gmail.com>
2024-03-21 00:07:49 +0200

    Kontaktinės formos modelio sukūrimas, lightbox mygtūkų įdiegimas ir css konfigūravimas

--- autoclub/autoclub/settings.py
+++ autoclub/autoclub/settings.py
@@ -136,6 +136,24 @@ LOGOUT_REDIRECT_URL = 'discussion'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
+TINYMCE_DEFAULT_CONFIG = {
+    "menubar": "file edit view insert format tools table help",
+    "plugins": "advlist autolink lists link image charmap anchor "
+               "searchreplace visualblocks code insertdatetime "
+               "media table paste help wordcount",
+    "toolbar": "undo redo | bold italic underline strikethrough | "
+               "fontselect fontsizeselect formatselect | "
+               "alignleft aligncenter alignright alignjustify | "
+               "outdent indent | numlist bullist checklist | "
+               "forecolor backcolor casechange permanentpen "
+               "formatpainter removeformat | pagebreak | "
+               "charmap emoticons | insertfile image media "
+               "pageembed link anchor codesample | "
+               "ltr rtl | showcomments addcomment code",
+    "custom_undo_redo_levels": 10,
+}
+
+
 try:
     from .local_settings import *
 except ImportError:
--- autoclub/website/models.py
+++ autoclub/website/models.py
@@ -1,3 +1,60 @@
+from typing import Iterable
 from django.db import models
+from django.utils.translation import gettext as _
+from django.urls import reverse
+from django.contrib.auth.models import User
 
-# Create your models here.
+
+SUBJECT_CHOICES = (
+    ('', _('-- Please choose --')),
+    ('Join', _('For those who want to join the club')),
+    ('Events', _('Official club events and organisational matters')),
+    ('Suggestions', _('Suggestions, complaints for club activities')),
+    ('merchandise', _('Club merchandise')),
+    ('Other', _('Other general issues related to the club\'s activities')),
+)
+
+TICKET_STATUSES = (
+    ('new', _('new')),
+    ('read', _('read')),
+    ('processing', _('processing')),
+    ('closed', _('closed')),
+)
+
+
+class Ticket(models.Model):
+    subject = models.CharField(_("subject"), max_length=100, choices=SUBJECT_CHOICES, default='')
+    content = models.TextField(_("content"), max_length=15000, default='', blank=True)
+    sender = models.ForeignKey(
+        User, 
+        verbose_name=_("sender"), 
+        on_delete=models.CASCADE, 
+        related_name='support_tickets', 
+        null=True, blank=True,
+    )
+    sender_name = models.CharField(_("full name"), max_length=100, null=True, blank=True)
+    sender_email = models.EmailField(_("email"), max_length=254, null=True, blank=True)
+    sent_at = models.DateTimeField(_("sent at"), auto_now_add=True, db_index=True)
+    mail_sent = models.BooleanField(_("email sent"), default=False)
+    status = models.CharField(_("status"), max_length=15, choices=TICKET_STATUSES, default='new', db_index=True)
+
+    class Meta:
+        verbose_name = _("ticket")
+        verbose_name_plural = _("tickets")
+        ordering = ['-sent_at']
+
+    def __str__(self):
+        return _("{} from {} sent {}").format(
+            self.subject,
+            self.sender_email,
+            self.sent_at,
+        )
+
+    def clean(self) -> None:
+        if self.sender:
+            self.sender_email = self.sender.email
+            self.sender_name = f"{self.sender.first_name} {self.sender.last_name}"
+
+    def get_absolute_url(self):
+        return reverse("ticket_detail", kwargs={"pk": self.pk})
+    
