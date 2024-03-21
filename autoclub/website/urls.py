fc40ba9fb4ff5ad80a7c923d7424406a3345a792
vokator <vokator@gmail.com>
2024-03-21 22:50:43 +0200

    Puslapio šablonų konfigūravimas ir stilizavimas

--- autoclub/website/urls.py
+++ autoclub/website/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path
 from . import views
 
-
 urlpatterns = [
     path('', views.HomeView.as_view(), name='home'),
     path('events/', views.EventsView.as_view(), name='events'),
