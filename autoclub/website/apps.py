adaee152d43340a0eec795488d83694644301cdb
vokator <vokator@gmail.com>
2024-03-23 14:42:53 +0200

    Aplikacijų parengimas vertimui

--- autoclub/website/apps.py
+++ autoclub/website/apps.py
@@ -5,4 +5,4 @@ from django.utils.translation import gettext_lazy as _
 class WebsiteConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'website'
-    verbose_name = 'website'
+    verbose_name = _('website')
