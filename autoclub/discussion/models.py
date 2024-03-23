58d19e0891971a934d6b79e19843ca3146d5cc1a
vokator <vokator@gmail.com>
2024-03-23 14:35:30 +0200

    Diskusijų modelio konfigūravimas

--- autoclub/discussion/models.py
+++ autoclub/discussion/models.py
@@ -6,7 +6,7 @@ from tinymce.models import HTMLField
 
 
 class Category(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=255, db_index=True)
 
     def __str__(self):
         return self.name
@@ -18,12 +18,12 @@ class Category(models.Model):
 class Post(models.Model):
     title = models.CharField(_("title"), max_length=255)
     header_image = models.ImageField(_("header image"), upload_to='images/', null=True, blank=True)
-    author = models.ForeignKey(User, verbose_name=_("author"), related_name='posts', on_delete=models.CASCADE)
+    author = models.ForeignKey(User, verbose_name=_("author"), related_name='posts', on_delete=models.CASCADE, db_index=True)
     content = HTMLField(verbose_name=_("content"))
     snippet = models.CharField(_("snippet"), max_length=255, null=True, blank=True)
     created_at = models.DateTimeField(_("created at"), auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(_("updated at"), auto_now=True, db_index=True)
-    category = models.ForeignKey(Category, verbose_name=_("category"), related_name='posts', on_delete=models.CASCADE, null=True)
+    category = models.ForeignKey(Category, verbose_name=_("category"), related_name='posts', on_delete=models.CASCADE, null=True, db_index=True)
 
     class Meta:
         verbose_name = _("post")
@@ -35,11 +35,11 @@ class Post(models.Model):
 
     def get_absolute_url(self):
         return reverse("article_detail", kwargs={"pk": self.pk})
-    
+
 
 class Comment(models.Model):
-    post = models.ForeignKey(Post, verbose_name=_("post"), related_name='comments', on_delete=models.CASCADE)
-    author = models.ForeignKey(User, verbose_name=_("author"), related_name='user_comments', on_delete=models.CASCADE, null=True)
+    post = models.ForeignKey(Post, verbose_name=_("post"), related_name='comments', on_delete=models.CASCADE, db_index=True)
+    author = models.ForeignKey(User, verbose_name=_("author"), related_name='user_comments', on_delete=models.CASCADE, null=True, db_index=True)
     content = HTMLField(verbose_name=_("content"))
     created_at = models.DateTimeField(_("created at"), auto_now_add=True, db_index=True)
     updated_at = models.DateTimeField(_("updated at"), auto_now=True, db_index=True)
@@ -54,4 +54,3 @@ class Comment(models.Model):
 
     def get_absolute_url(self):
         return reverse('article_detail', kwargs={'pk': self.post.pk})
-    
