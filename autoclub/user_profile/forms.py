ec8c79c1686ec25866ad0262532ee9cbff9fda98
vokator <vokator@gmail.com>
2024-03-25 15:02:48 +0200

    Vertimų konfigūravimas vartotojo profilyje

--- autoclub/user_profile/forms.py
+++ autoclub/user_profile/forms.py
@@ -6,9 +6,9 @@ from user_profile.models import Profile
 
 
 class CreateUserForm(UserCreationForm):
-    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'custom-form-control', 'placeholder': 'Please enter your First Name'}))
-    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'class': 'custom-form-control', 'placeholder': 'Please enter your Last Name'}))
-    email = forms.EmailField(widget=forms.EmailInput(attrs={'class': 'custom-form-control', 'placeholder': 'Please enter your e-mail address'}))
+    first_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': _('Please enter your First Name')}))
+    last_name = forms.CharField(max_length=100, widget=forms.TextInput(attrs={'placeholder': _('Please enter your Last Name')}))
+    email = forms.EmailField(widget=forms.EmailInput(attrs={'placeholder': _('Please enter your e-mail address')}))
 
     class Meta:
         model = User
@@ -16,46 +16,46 @@ class CreateUserForm(UserCreationForm):
 
     def __init__(self, *args, **kwargs):
         super(CreateUserForm, self).__init__(*args, **kwargs)
-        self.fields['username'].label = 'Username:'
-        self.fields['username'].widget.attrs.update({'placeholder': 'Please enter username'})
-        self.fields['first_name'].label = 'First Name:'
-        self.fields['last_name'].label = 'Last Name:'
-        self.fields['email'].label = 'Email:'
-        self.fields['password1'].label = 'Password:'
-        self.fields['password1'].widget.attrs.update({'placeholder': 'Please enter the password you created'})
-        self.fields['password2'].label = 'Password confirmation:'
-        self.fields['password2'].widget.attrs.update({'placeholder': 'Please enter the password you created and confirm'})
+        self.fields['username'].label = _('Username:')
+        self.fields['username'].widget.attrs.update({'placeholder': _('Please enter username')})
+        self.fields['first_name'].label = _('First Name:')
+        self.fields['last_name'].label = _('Last Name:')
+        self.fields['email'].label = _('Email:')
+        self.fields['password1'].label = _('Password:')
+        self.fields['password1'].widget.attrs.update({'placeholder': _('Please enter the password you created')})
+        self.fields['password2'].label = _('Password confirmation:')
+        self.fields['password2'].widget.attrs.update({'placeholder': _('Please enter the password you created and confirm')})
 
 
 class ProfilePageForm(forms.ModelForm):
     bio = forms.CharField(
         label=_('Bio'), 
-        widget=forms.Textarea(attrs={'class': 'custom-form-control', 'placeholder': _('Please tell us more about yourself')}),
+        widget=forms.Textarea(attrs={'placeholder': _('Please tell us more about yourself')}),
         required=False
     )
     website_url = forms.CharField(
         label=_('Website url'),
-        widget=forms.TextInput(attrs={'class': 'custom-form-control', 'placeholder': _('Your website page (if you have one)')}),
+        widget=forms.TextInput(attrs={'placeholder': _('Your website page (if you have one)')}),
         required=False
     )
     facebook_url = forms.CharField(
         label=_('Facebook url'),
-        widget=forms.TextInput(attrs={'class': 'custom-form-control', 'placeholder': _('Your Facebook page (if you have one)')}),
+        widget=forms.TextInput(attrs={'placeholder': _('Your Facebook page (if you have one)')}),
         required=False
     )
     twitter_url = forms.CharField(
         label=_('Twitter url'),
-        widget=forms.TextInput(attrs={'class': 'custom-form-control', 'placeholder': _('Your Twitter page (if you have one)')}),
+        widget=forms.TextInput(attrs={'placeholder': _('Your Twitter page (if you have one)')}),
         required=False
     )
     instagram_url = forms.CharField(
         label=_('Instagram url'),
-        widget=forms.TextInput(attrs={'class': 'custom-form-control', 'placeholder': _('Your Instagram page (if you have one)')}),
+        widget=forms.TextInput(attrs={'placeholder': _('Your Instagram page (if you have one)')}),
         required=False
     )
     pinterest_url = forms.CharField(
         label=_('Pinterest url'),
-        widget=forms.TextInput(attrs={'class': 'custom-form-control', 'placeholder': _('Your Pinterest page (if you have one)')}),
+        widget=forms.TextInput(attrs={'placeholder': _('Your Pinterest page (if you have one)')}),
         required=False
     )
 
@@ -68,16 +68,16 @@ class EditProfileForm(UserChangeForm):
     first_name = forms.CharField(
         label=_('First Name'),
         max_length=100, 
-        widget=forms.TextInput(attrs={'class': 'custom-form-control', 'placeholder': _('Please enter your First Name')})
+        widget=forms.TextInput(attrs={'placeholder': _('Please enter your First Name')})
     )
     last_name = forms.CharField(
         label=_('Last Name'),
         max_length=100, 
-        widget=forms.TextInput(attrs={'class': 'custom-form-control', 'placeholder': _('Please enter your Last Name')})
+        widget=forms.TextInput(attrs={'placeholder': _('Please enter your Last Name')})
     )
     email = forms.EmailField(
         label=_('Email'),
-        widget=forms.EmailInput(attrs={'class': 'custom-form-control', 'placeholder': _('Please enter your e-mail address')})
+        widget=forms.EmailInput(attrs={'placeholder': _('Please enter your e-mail address')})
     )
     last_login = forms.CharField(
         label=_('Last login'),
